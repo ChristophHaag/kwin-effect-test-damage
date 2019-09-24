49289f4b2dc4b3283d44b7ba6901c7bf435fdcdd
Christoph Haag <christoph.haag@collabora.com>
2019-09-24 18:03:28 +0200

    initial commit

---
+++ src/TestDamage.cpp
@@ -0,0 +1,94 @@
+#include "TestDamage.h"
+
+#include <KGlobalAccel>
+#include <KLocalizedString>
+#include <kwindowsystem.h>
+#include <kwinglplatform.h>
+#include <kwinglutils.h>
+#include <QAction>
+#include <QFile>
+#include <QMatrix4x4>
+#include <QStandardPaths>
+#include <QTimer>
+#include <QtDebug>
+#include <QProcessEnvironment>
+
+
+KWIN_EFFECT_FACTORY_SUPPORTED_ENABLED(TestDamageFactory,
+                                      TestDamage,
+                                      "test_damage.json",
+                                      return TestDamage::supported();
+                                      , return TestDamage::enabledByDefault();)
+
+TestDamage::TestDamage()
+{
+    testWindowCaption = QProcessEnvironment::systemEnvironment().value("TEST_DAMAGE_CAPTION", "glxgears");
+    qDebug() << "Init test damage plugin for window" << testWindowCaption;
+
+    connect(KWin::effects, SIGNAL(windowDamaged(KWin::EffectWindow *, QRect)), SLOT(damaged(KWin::EffectWindow *)));
+    connect(KWin::effects, &KWin::EffectsHandler::windowClosed, this, &TestDamage::slotWindowClosed);
+    connect(KWin::effects, &KWin::EffectsHandler::windowAdded, this, &TestDamage::slotWindowAdded);
+}
+
+TestDamage::~TestDamage()
+{
+}
+
+bool TestDamage::supported()
+{
+    return true;
+}
+
+bool TestDamage::enabledByDefault()
+{
+    return true;
+}
+
+void TestDamage::drawWindow(KWin::EffectWindow *w,
+                            int mask,
+                            QRegion region,
+                            KWin::WindowPaintData &data)
+{
+    //qDebug() << "Draw Window" << w->caption();
+
+    if (testWindowCaption == w->caption()) {
+        if (!damageGotten)
+            qDebug() << "DrawWindow for window" << w->caption() << "without previous damaged event!";
+
+        damageGotten = false;
+    }
+
+    KWin::effects->drawWindow(w, mask, region, data);
+}
+
+void TestDamage::damaged(KWin::EffectWindow *w)
+{
+    if (testWindowCaption == w->caption()) {
+        damageGotten = true;
+    }
+}
+
+void TestDamage::postPaintWindow(KWin::EffectWindow *w)
+{
+    KWin::effects->postPaintWindow(w);
+}
+
+void TestDamage::slotWindowClosed(KWin::EffectWindow *w)
+{
+}
+
+void TestDamage::slotWindowAdded(KWin::EffectWindow *w)
+{
+}
+
+void TestDamage::prePaintScreen(KWin::ScreenPrePaintData &data, int time)
+{
+    KWin::effects->prePaintScreen(data, time);
+}
+
+void TestDamage::postPaintScreen()
+{
+    KWin::effects->postPaintScreen();
+}
+
+#include "TestDamage.moc"
---
+++ src/TestDamage.h
@@ -0,0 +1,32 @@
+#ifndef EFFECTS_TEST_DAMAGE_H
+#define EFFECTS_TEST_DAMAGE_H
+
+#include <kwineffects.h>
+
+class TestDamage : public KWin::Effect
+{
+    Q_OBJECT
+public:
+    ~TestDamage();
+    TestDamage();
+    static bool supported();
+    static bool enabledByDefault();
+
+    void postPaintWindow(KWin::EffectWindow *w);
+    void drawWindow(KWin::EffectWindow *w, int mask, QRegion region, KWin::WindowPaintData &data);
+    void prePaintScreen(KWin::ScreenPrePaintData &data, int time);
+    void postPaintScreen();
+private:
+    QString testWindowCaption;
+    bool damageGotten = false;
+
+public Q_SLOTS:
+    void slotWindowClosed(KWin::EffectWindow *w);
+    void slotWindowAdded(KWin::EffectWindow *w);
+    void damaged(KWin::EffectWindow *w);
+
+Q_SIGNALS:
+
+};
+
+#endif
